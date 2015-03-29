5f9d5e0b158f32f4cc2d820a59356569594efd3c
Serg Krivoblotsky <krivoblotsky@me.com>
2015-03-29 16:27:45 +0300

    Init

---
+++ AutolayoutCenter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutolayoutCenter
+//
+//  Created by Sergii Kryvoblotskyi on 3/29/15.
+//  Copyright (c) 2015 Sergii Kryvoblotskyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutolayoutCenter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutolayoutCenter
+//
+//  Created by Sergii Kryvoblotskyi on 3/29/15.
+//  Copyright (c) 2015 Sergii Kryvoblotskyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
